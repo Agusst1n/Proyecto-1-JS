79fa7f47948216c5264d3eed6b078645c90cee0c
Nitsuga <88418234+Agusst1n@users.noreply.github.com>
2022-05-13 01:21:04 -0300

    funcionamiento de personajes 90%

---
+++ pages/personajes/characterFetch.js
@@ -0,0 +1,79 @@
+//!ESTE MUESTRA LAS CARACTERISTICAS DEL PERSONAJE QUE ELEGISTE 
+
+const urlActual = window.location.hash //con esto obtengo la URL Actual
+console.log(urlActual)
+let id = urlActual.slice(2,38) //con el slice le saco los 2 primeros caracteres que son un '#/'
+console.log(id)
+
+// let id = urlActual
+// id.split(' ')
+// console.log(id)
+
+
+
+export default ()=>{
+    //toda via falta trabajo aca
+    // const div_card = document.createElement('div')
+    fetch(`https://valorant-api.com/v1/agents/${id}`)
+    .then(res => res.json())
+    .then(info => {
+        let data =info.data
+        console.log(data)
+        
+
+        //SOUND
+        let idSound = data.voiceLine.mediaList[0].id
+        // console.log(data.voiceLine.mediaList[0].id)
+        let sonido = new Audio(); //creo un sonido
+        sonido.src = `../sounds/${idSound}.wav` //le digo que el sonido va a ser dinamico, va a ser segun el personaje que hayamos seleccionado
+
+
+        let div_card = document.createElement('div') //creo un div
+        let div_image = document.createElement('div') //creo otro div
+        div_image.className = 'div_card_img' //le asigno una clase, este div va a contener la imagen
+        div_card.className = 'card_character_estadistics' // y este div va a contener los datos del personaje
+        let img = document.createElement('img') //creo una img
+        let p = document.createElement('p')//creo un p
+        let h2 = document.createElement('h2')//creo un h2
+        let h4 = document.createElement('h4')//creo un h4
+        let button = document.createElement('button') //creo un boton
+        button.textContent = 'Voice' //le digo que el boton va a tener escrito la palabra Voice
+        button.className = 'button_sound' //le agrego una clase al boton
+        button.onclick = () =>{ //le agreo un evento para que cuando le hagan click ejecute el sonido con la voz del personaje seleccionado
+            console.log('click')
+            sonido.play()
+        }
+
+        h4.textContent = `Role: ${data.role.displayName}` //le asigno contenido al h4
+        // console.log(data.role.displayName)
+        h2.textContent = `${data.displayName}` //le asigno contenido al h2
+        // img.src = `${data.bustPortrait}`
+        console.log(id, 'id')
+        img.src = `../assets/characters_img/${id}.png` //le asigno contenido a la img
+        img.className = 'characters_img' //le asigno una clase
+        p.textContent = `${data.description}` //le asino contenido al p
+        div_card.appendChild(h2) //le agreo el h2
+        div_card.appendChild(h4) //le agrego el h4
+        // div.appendChild(img)
+        div_card.appendChild(p) //le agrego el p
+        div_card.appendChild(button) //le agrego el boton
+        div_image.appendChild(img) //le agrego la imagen
+        div.appendChild(div_image) //agrego el div que contniene la imagen en otro div padre
+        div.appendChild(div_card) //agrego el div que contiene la informacion en el mismo div padre
+
+        // setTimeout(()=>{
+        //     location.reload();
+        //     console.log('timeout')
+        // },1000)
+
+    })
+
+    const characterFetch = `
+        <h1>CharacterFetch</h1>
+    `;
+    const div = document.createElement('div'); //mete todo dentro de este div
+    div.className = 'character_estadistics' //le agrego una clase a ese div padre
+    // div.innerHTML = div_card;
+
+    return div // lo retorno y como esta en una funcion  que se puede exportar, lo voy a importar en el index router para que pueda ser renderizado
+}
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,8 @@
-import characters from '../pages/characters.js'
+import characters from '../pages/personajes/characters.js'
 import home from '../pages/home.js'
-import armas from '../pages/armas.js'
-import mapas from '../pages/mapas.js'
-import characterFetch from '../pages/characterFetch.js'
+import armas from '../pages/armas/armas.js'
+import mapas from '../pages/mapas/mapas.js'
+import characterFetch from '../pages/personajes/characterFetch.js' //importo el caracterFetch
 import overlay from '../overlay/overlay.js'
 import overlay2 from '../overlay/overlay2.js'
 import overlay3 from '../overlay/overlay3.js'
@@ -19,99 +19,99 @@ const router = (route)=>{
     root.textContent = '' //lo que hace es que cada vez que entre no se vuelva a pintar lo mismo una y otra vez
 
     switch (route){
-        case '#/':{
+        case '#/':{ // si esta en la ruta principal quiero que
             // root.appendChild(overlay());
             document.body.appendChild(overlay())
-            document.body.appendChild(overlay2())
+            document.body.appendChild(overlay2()) //renderize los 3 overlay
             document.body.appendChild(overlay3())
-            root.appendChild(home())
-            document.body.appendChild(hoverAnimation())
-            document.body.appendChild(script())
+            root.appendChild(home()) //que al root le agregue el componente home (contiene las 3 cards [armas,personajes y mapas])
+            document.body.appendChild(hoverAnimation()) //le digo que las cards van a tener la animacion del hover
+            document.body.appendChild(script()) //y aca esta su script para que funcionen
             break;
         }
-        case '#/dade69b4-4f5a-8528-247b-219e5a1facd6':{
-            return root.appendChild(characterFetch())
+        case '#/dade69b4-4f5a-8528-247b-219e5a1facd6':{  //*cuando este en esta ruta fade
+            return root.appendChild(characterFetch())//me va agregar al root el componente charactersFetch el cual trae todos los datos de ese personaje
         }
-        case '#/5f8d3a7f-467b-97f3-062c-13acf203c006':{
-            return root.appendChild(characterFetch())
+        case '#/5f8d3a7f-467b-97f3-062c-13acf203c006':{ //*cuando este en esta ruta breach
+            return root.appendChild(characterFetch())//me va agregar al root el componente charactersFetch el cual trae todos los datos de ese personaje
         }
-        case '#/f94c3b30-42be-e959-889c-5aa313dba261':{
-            return root.appendChild(characterFetch())
+        case '#/f94c3b30-42be-e959-889c-5aa313dba261':{ //*cuando este en esta ruta raze
+            return root.appendChild(characterFetch())//me va agregar al root el componente charactersFetch el cual trae todos los datos de ese personaje
         }
-        case '#/armas':{
+        case '#/armas':{//cuando este en la ruta armas
+            document.body.appendChild(overlay()) 
+            document.body.appendChild(overlay2()) //van a cargar los 3 overlays
+            document.body.appendChild(overlay3())
+            root.appendChild(armas()) //agrego al root el componente armas
+            document.body.appendChild(script()) //este script hace que los overlay funcionen
+            break;
+        }
+        case '#/personajes':{ //cuando este en la ruta personajes
             document.body.appendChild(overlay())
-            document.body.appendChild(overlay2())
+            document.body.appendChild(overlay2())//van a cargar los 3 overlays
             document.body.appendChild(overlay3())
-            root.appendChild(armas())
-            document.body.appendChild(script())
+            root.appendChild(characters())//agrego al root el componente personajes
+            document.body.appendChild(script())//este script hace que los overlay funcionen
             break;
         }
-        case '#/personajes':{
+        case '#/mapas':{//cuando este en la ruta mapas
             document.body.appendChild(overlay())
-            document.body.appendChild(overlay2())
+            document.body.appendChild(overlay2())//van a cargar los 3 overlays
             document.body.appendChild(overlay3())
-            root.appendChild(characters())
-            document.body.appendChild(script())
+            root.appendChild(mapas())//agrego al root el componente mapas
+            document.body.appendChild(script())//este script hace que los overlay funcionen
             break;
         }
-        case '#/mapas':{
-            document.body.appendChild(overlay())
-            document.body.appendChild(overlay2())
-            document.body.appendChild(overlay3())
-            root.appendChild(mapas())
-            document.body.appendChild(script())
-            break;
-        }
-        case '#/333':{
+        case '#/333':{ //ruta de prueba
             return root.appendChild(characterFetch())
         }
-        case '#/22697a3d-45bf-8dd7-4fec-84a9e28c69d7':{
-            return root.appendChild(characterFetch())
+        case '#/22697a3d-45bf-8dd7-4fec-84a9e28c69d7':{ //*cuando este en esta ruta (chamber)
+            return root.appendChild(characterFetch())//me va agregar al root el componente charactersFetch el cual trae todos los datos de ese personaje
         }
-        case '#/601dbbe7-43ce-be57-2a40-4abd24953621':{
-            return root.appendChild(characterFetch())
+        case '#/601dbbe7-43ce-be57-2a40-4abd24953621':{//*cuando este en esta ruta kayo
+            return root.appendChild(characterFetch())//me va agregar al root el componente charactersFetch el cual trae todos los datos de ese personaje
         }
-        case '#/6f2a04ca-43e0-be17-7f36-b3908627744d':{
-            return root.appendChild(characterFetch())
+        case '#/6f2a04ca-43e0-be17-7f36-b3908627744d':{ //*cuando este en esta ruta skye
+            return root.appendChild(characterFetch())//me va agregar al root el componente charactersFetch el cual trae todos los datos de ese personaje
         }
-        case '#/117ed9e3-49f3-6512-3ccf-0cada7e3823b':{
-            return root.appendChild(characterFetch())
+        case '#/117ed9e3-49f3-6512-3ccf-0cada7e3823b':{//*cuando este en esta ruta cypher
+            return root.appendChild(characterFetch())//me va agregar al root el componente charactersFetch el cual trae todos los datos de ese personaje
         }
-        case '#/320b2a48-4d9b-a075-30f1-1f93a9b638fa':{
-            return root.appendChild(characterFetch())
+        case '#/320b2a48-4d9b-a075-30f1-1f93a9b638fa':{//*cuando este en esta ruta sova
+            return root.appendChild(characterFetch())//me va agregar al root el componente charactersFetch el cual trae todos los datos de ese personaje
         }
-        case '#/1e58de9c-4950-5125-93e9-a0aee9f98746':{
-            return root.appendChild(characterFetch())
+        case '#/1e58de9c-4950-5125-93e9-a0aee9f98746':{//*cuando este en esta ruta killjoy
+            return root.appendChild(characterFetch())//me va agregar al root el componente charactersFetch el cual trae todos los datos de ese personaje
         }
-        case '#/707eab51-4836-f488-046a-cda6bf494859':{
-            return root.appendChild(characterFetch())
+        case '#/707eab51-4836-f488-046a-cda6bf494859':{ //*cuando este en esta ruta viper
+            return root.appendChild(characterFetch())//me va agregar al root el componente charactersFetch el cual trae todos los datos de ese personaje
         }
-        case '#/eb93336a-449b-9c1b-0a54-a891f7921d69':{
-            return root.appendChild(characterFetch())
+        case '#/eb93336a-449b-9c1b-0a54-a891f7921d69':{//*cuando este en esta ruta phoenix
+            return root.appendChild(characterFetch())//me va agregar al root el componente charactersFetch el cual trae todos los datos de ese personaje
         }
-        case '#/41fb69c1-4189-7b37-f117-bcaf1e96f1bf':{
-            return root.appendChild(characterFetch())
+        case '#/41fb69c1-4189-7b37-f117-bcaf1e96f1bf':{ //*cuando este en esta ruta astra
+            return root.appendChild(characterFetch())//me va agregar al root el componente charactersFetch el cual trae todos los datos de ese personaje
         }
-        case '#/9f0d8ba9-4140-b941-57d3-a7ad57c6b417':{
-            return root.appendChild(characterFetch())
+        case '#/9f0d8ba9-4140-b941-57d3-a7ad57c6b417':{//*cuando este en esta ruta brimstone
+            return root.appendChild(characterFetch())//me va agregar al root el componente charactersFetch el cual trae todos los datos de ese personaje
         }
-        case '#/bb2a4828-46eb-8cd1-e765-15848195d751':{
-            return root.appendChild(characterFetch())
+        case '#/bb2a4828-46eb-8cd1-e765-15848195d751':{ //*cuando este en esta ruta neon
+            return root.appendChild(characterFetch())//me va agregar al root el componente charactersFetch el cual trae todos los datos de ese personaje
         }
-        case '#/7f94d92c-4234-0a36-9646-3a87eb8b5c89':{
-            return root.appendChild(characterFetch())
+        case '#/7f94d92c-4234-0a36-9646-3a87eb8b5c89':{//*cuando este en esta ruta yoru
+            return root.appendChild(characterFetch())//me va agregar al root el componente charactersFetch el cual trae todos los datos de ese personaje
         }
-        case '#/569fdd95-4d10-43ab-ca70-79becc718b46':{
-            return root.appendChild(characterFetch())
+        case '#/569fdd95-4d10-43ab-ca70-79becc718b46':{//*cuando este en esta ruta sage
+            return root.appendChild(characterFetch())//me va agregar al root el componente charactersFetch el cual trae todos los datos de ese personaje
         }
-        case '#/a3bfb853-43b2-7238-a4f1-ad90e9e46bcc':{
-            return root.appendChild(characterFetch())
+        case '#/a3bfb853-43b2-7238-a4f1-ad90e9e46bcc':{//*cuando este en esta ruta reyna
+            return root.appendChild(characterFetch())//me va agregar al root el componente charactersFetch el cual trae todos los datos de ese personaje
         }
-        case '#/8e253930-4c05-31dd-1b6c-968525494517':{
-            return root.appendChild(characterFetch())
+        case '#/8e253930-4c05-31dd-1b6c-968525494517':{//*cuando este en esta ruta omen
+            return root.appendChild(characterFetch())//me va agregar al root el componente charactersFetch el cual trae todos los datos de ese personaje
         }
-        case '#/add6443a-41bd-e414-f6ad-e58d267f4e95':{
-            return root.appendChild(characterFetch())
+        case '#/add6443a-41bd-e414-f6ad-e58d267f4e95':{//*cuando este en esta ruta jett
+            return root.appendChild(characterFetch())//me va agregar al root el componente charactersFetch el cual trae todos los datos de ese personaje
         }
         default: return console.log('404 Notfound ')
     }
